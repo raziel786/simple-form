0c67eb5a327fba856a358208d3bf62f3d5679e9c
Mubean Hussain <mub33n@hotmail.co.uk>
2025-05-26 18:27:53 +0100

    setup base server with necessary packages

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,21 @@
-console.log("Hello from Node!");
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+/**
+ * cors is needed to say it is
+ * okay for other sites (like the client/frontend) 
+ * to talk to this server.
+ */
+app.use(cors());
+
+app.use(bodyParser.json());
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
